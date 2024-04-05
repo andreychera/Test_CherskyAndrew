b134104136a3a9ebc8350098bf7a91636d4fe47e
andreychera <andreychera@knu.ua>
2024-04-05 11:17:04 +0300

    Added a main function

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    input_file_name = "input.txt"
+    output_file_name = "filtered.txt"
+    keyword = input("Введіть ключове слово для фільтрації: ")
+
+
+
+if __name__ == "__main__":
+    main()
